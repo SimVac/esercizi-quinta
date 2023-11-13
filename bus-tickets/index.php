00ce39e8b4b1f765a41b64d336f4846fea2129d8
SimVac <simonevacca932@gmail.com>
2023-11-13 10:50:42 +0100

    Finisce la logica del progetto bus-tickets

--- bus-tickets/index.php
+++ bus-tickets/index.php
@@ -2,6 +2,34 @@
 
 require 'vendor/autoload.php';
 
+function genera_codice()
+{
+    $codice = '';
+    for ($i = 0; $i < 18; $i++) {
+        $codice .= rand()%10;
+    }
+    return $codice;
+}
+
+$tipi = [
+    1 => 1,
+    2 => 1.5,
+    3 => 2
+];
+
+$nome_tipi = [
+    1 => 'Biglietto singolo',
+    2 => 'Biglietto orario',
+    3 => 'Biglietto giornaliero'
+];
+
+$zone = [
+    1 => 1.4,
+    2 => 1.8,
+    3 => 2.2
+];
+
+
 $template = new League\Plates\Engine('templates', 'tpl');
 
 if (isset($_POST['nominativo'])){
@@ -9,5 +37,20 @@ if (isset($_POST['nominativo'])){
     $zona = $_POST['zona'];
     $tipo = $_POST['tipo'];
     $quantita = $_POST['quantita'];
+
+    $tickets = [];
+    for ($i = 0; $i < $quantita; $i++){
+        $ticket = [];
+        $ticket['nominativo'] = $nominativo;
+        $ticket['zona'] = $zona;
+        $ticket['tipo'] = $nome_tipi[$tipo];
+        $ticket['codice'] = genera_codice();
+        $ticket['costo'] = number_format($zone[$zona] * $tipi[$tipo], 2, '.', '');
+        $ticket['data'] = date('d/m/Y H:i:s');
+
+        $tickets[] = $ticket;
+    }
+
+    echo $template->render('bus-tickets', ['tickets'=>$tickets]);
 }else
-    echo $template->render('bus-form', []);
+    echo $template->render('bus-form');
--- bus-tickets/templates/bus-tickets.tpl
+++ bus-tickets/templates/bus-tickets.tpl
@@ -5,6 +5,15 @@
     <title>Bus Tickets</title>
 </head>
 <body>
-
+    <?php foreach ($tickets as $ticket):?>
+        <div>
+            <h2><?=$ticket['nominativo']?></h2>
+            <img src="https://c8.alamy.com/compit/prah60/creative-logo-autotrasporti-concept-design-dei-modelli-prah60.jpg" alt="Logo dell'azienda di trasporti" width="100" height="100">
+            <p>Data e ora di acquisto: <?=$ticket['data']?></p>
+            <p>Costo: <?=$ticket['costo']?>€</p>
+            <p>Codice: <?=$ticket['codice']?></p>
+            <p><?=$ticket['tipo']?> in zona <?=$ticket['zona']?></p>
+        </div>
+    <?php endforeach; ?>
 </body>
 </html>
